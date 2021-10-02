fcf7c7110380d384e079938d2c79068ff2d0b79a
Megatron8000 <barykin88@hotmail.com>
2021-10-02 08:56:57 +0400

    add comments

--- src/com/java/hello/module3/Bus.java
+++ src/com/java/hello/module3/Bus.java
@@ -1,10 +1,10 @@
 package com.java.hello.module3;
 
 public class Bus {
-   public String model;
+   public String model; //поля класса - описание свойств объекта
    public String color;
 
-    public void showColor(){
+    public void showColor(){  //методы класса - описание поведения объекта
         System.out.println(color);
     }
 }
