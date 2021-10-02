801d9b4e24c71bd72f682585d075ccc99e4614cb
Megatron8000 <barykin88@hotmail.com>
2021-10-02 09:08:41 +0400

    add comments

--- src/com/java/hello/module1/Main.java
+++ src/com/java/hello/module1/Main.java
@@ -1,7 +1,19 @@
 package com.java.hello.module1;
 
+import com.java.hello.module3.Bus;
+
 public class Main {
     public static void main(String[] args) {
+        Bus firstBus = new Bus(); //объявление и ининциализвция нового объекта firstBus класса Bus
+        Bus secondBus = new Bus();
+        Bus anotherBus = new Bus();
 
+        firstBus.color = "Red";
+        secondBus.color = "Yellow";
+        anotherBus.color = "Blue";
+
+        firstBus.showColor();
+        secondBus.showColor();
+        anotherBus.showColor();
     }
 }
