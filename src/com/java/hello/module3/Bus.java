e686cbebb7e720e761a9a4ed92897c73d019d2bb
Megatron8000 <barykin88@hotmail.com>
2021-10-02 08:54:01 +0400

    class object method

---
+++ src/com/java/hello/module3/Bus.java
@@ -0,0 +1,10 @@
+package com.java.hello.module3;
+
+public class Bus {
+   public String model;
+   public String color;
+
+    public void showColor(){
+        System.out.println(color);
+    }
+}
