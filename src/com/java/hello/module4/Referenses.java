0bdd04f8103d2acc0e993a51aab1a84c7da2aff5
Megatron8000 <barykin88@hotmail.com>
2021-10-06 19:42:05 +0400

    String type in java

--- src/com/java/hello/module4/Referenses.java
+++ src/com/java/hello/module4/Referenses.java
@@ -1,6 +1,15 @@
 package com.java.hello.module4;
 
 public class Referenses {
-    String str = "Asshole";
-    String str2 = new String("Asshole");
+   static String str = "Asshole";
+   static String str2 = new String("Asshole");
+
+    public void sample(){
+        new String(str);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(str2);
+        System.out.println(str);
+    }
 }
