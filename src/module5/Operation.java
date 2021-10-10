b723188e1af4572dac4c953b64db11594557f36f
Megatron8000 <barykin88@hotmail.com>
2021-10-10 12:31:02 +0400

    addition sample

--- src/module5/Operation.java
+++ src/module5/Operation.java
@@ -1,4 +1,11 @@
 package module5;
 
 public class Operation {
+    int a = 5, b = 6, c;
+    String s1 = "Hot ", s2 = "Dog", result;
+
+    public void foo(){
+        c = a + b;
+        result = s1 + s2;
+    }
 }
