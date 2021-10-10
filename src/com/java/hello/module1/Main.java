4732eb5efe3316f674f80adde463c86b8017c062
Megatron8000 <barykin88@hotmail.com>
2021-10-10 12:42:09 +0400

    addition sample, run in main new method

--- src/com/java/hello/module1/Main.java
+++ src/com/java/hello/module1/Main.java
@@ -1,11 +1,13 @@
 package com.java.hello.module1;
 
-//import com.java.hello.module3.Bus;
+import com.java.hello.module3.Bus;
+
 
 public class Main {
     public static void main(String[] args) {
+        /*Operation oper = new Operation();
+        oper.foo();*/
 
-
-
+        Bus
     }
 }
--- src/module5/Operation.java
+++ src/module5/Operation.java
@@ -1,5 +1,6 @@
 package module5;
 
+
 public class Operation {
     int a = 5, b = 6, c;
     String s1 = "Hot ", s2 = "Dog", result;
