8a8fca14d3592bcf3ce0a7b6bc0ea276b72f7aaf
Megatron8000 <barykin88@hotmail.com>
2021-10-12 19:38:13 +0400

    fixed a typo

--- src/com/java/hello/module6/ControlOperations.java
+++ src/com/java/hello/module6/ControlOperations.java
@@ -2,15 +2,16 @@ package com.java.hello.module6;
 
 public class ControlOperations {
     int a = 5, b = 9;
-    boolean value = true;
+    boolean value = false;
 
     public void foo(){
         if (value){
             System.out.println("True");
             a++;
+            if (a<b){
+                System.out.println("Still true");
         }
-        if (a<b){
-            System.out.println("Still true");
+
         }
         else{
             System.out.println("False");
