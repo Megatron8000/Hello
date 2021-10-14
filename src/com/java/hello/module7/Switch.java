bf8bca2f2e250747bdcaa78220d6c4716cdb7fa7
Megatron8000 <barykin88@hotmail.com>
2021-10-14 19:15:18 +0400

    switch - case tutorial

--- src/com/java/hello/module7/Switch.java
+++ src/com/java/hello/module7/Switch.java
@@ -1,4 +1,16 @@
 package com.java.hello.module7;
 
 public class Switch {
+    int a = 2, b = 4, c = 2, d = 9;
+
+    public void foo() {
+        switch (55){
+            case 32:
+                System.out.println(32);
+            case 65:
+                System.out.println(65);
+            case 55:
+                System.out.println(55);
+        }
+    }
 }
