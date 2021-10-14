621235e94cb3a7ebaa670522453dedc6cff249ff
Megatron8000 <barykin88@hotmail.com>
2021-10-14 19:19:29 +0400

    run other sample switch in main

--- src/com/java/hello/module7/Switch.java
+++ src/com/java/hello/module7/Switch.java
@@ -4,13 +4,16 @@ public class Switch {
     int a = 2, b = 4, c = 2, d = 9;
 
     public void foo() {
-        switch (55){
+        switch (66){
             case 32:
                 System.out.println(32);
+                break;
             case 65:
                 System.out.println(65);
             case 55:
                 System.out.println(55);
+            default:
+                System.out.println("JOPA");
         }
     }
 }
