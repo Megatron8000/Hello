a82ba7022b7d11b947c771571656a75b7de9bc32
Megatron8000 <barykin88@hotmail.com>
2021-10-16 09:08:08 +0400

    while true cycle

--- src/com/java/hello/module8/Loop.java
+++ src/com/java/hello/module8/Loop.java
@@ -1,5 +1,11 @@
 package com.java.hello.module8;
 
 public class Loop {
+    int a = 5, b =7;
 
+    public void foo(){
+        while (a<b){
+            System.out.println(a);
+        }
+    }
 }
