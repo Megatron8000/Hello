f2c5a3d7b7b5ce73834158b13c8a3ee7db7e24f6
Megatron8000 <barykin88@hotmail.com>
2021-10-16 09:30:19 +0400

    run do while  cycle whit other params

--- src/com/java/hello/module8/Loop.java
+++ src/com/java/hello/module8/Loop.java
@@ -4,10 +4,13 @@ public class Loop {
     int a = 5, b =70;
 
     public void foo(){
-        while (a<b){ //цикл будет выполняться пока выражение - истина
+        /*while (a<b){ //цикл будет выполняться пока выражение - истина
             System.out.println(a);
-            a++;
+            a++;*/
 
-        }
+        do {
+            a++;
+            System.out.println(a);
+        } while (a<b);
     }
 }
