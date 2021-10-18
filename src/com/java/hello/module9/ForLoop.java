e6b7eef9434ddfcfc85ecad7f7c3a46d91ca3e87
Megatron8000 <barykin88@hotmail.com>
2021-10-18 19:51:34 +0400

    example of cycles for, for each

--- src/com/java/hello/module9/ForLoop.java
+++ src/com/java/hello/module9/ForLoop.java
@@ -1,9 +1,17 @@
 package com.java.hello.module9;
 
 public class ForLoop {
-    public  void foo(){
-        for (int i = 0; i<=20;i++){
+    int[] array = {1, 2, 3, 7, 4, 5, 8};
+
+    public void foo() {
+        for (int i = 0; i <= 10; i =i+2) {
             System.out.println(i);
+        }
+        for (int e : array) {
+            System.out.println(e);
         }
     }
 }
+
+
+
