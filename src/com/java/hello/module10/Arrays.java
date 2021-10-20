119e29df2eeb7d98df45e4fb3fba9ecef1038675
Megatron8000 <barykin88@hotmail.com>
2021-10-20 21:47:36 +0400

    arrays initialization sample

--- src/com/java/hello/module10/Arrays.java
+++ src/com/java/hello/module10/Arrays.java
@@ -3,4 +3,18 @@ package com.java.hello.module10;
 public class Arrays {
     int[] array = new int[5]; //массив примитивных типов
     Element[] box = new Element[10]; //массив ссылочных типов
+
+    int[] sample = {55, 66, 77};  //пример объявления массива с инициализацией
+
+    Element eFirst = new Element();
+    Element eSecond = new Element();
+    Element eThird = new Element();
+
+    Element[] pack = {eFirst, eSecond, eThird};
+
+
+    public void foo(){
+
+    }
 }
+
