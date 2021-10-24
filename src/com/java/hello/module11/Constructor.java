daf3bd99c0a32dea9b621fed5d6f761cc31ffc81
Megatron8000 <barykin88@hotmail.com>
2021-10-24 21:20:17 +0400

    write sample of Constructor

--- src/com/java/hello/module11/Constructor.java
+++ src/com/java/hello/module11/Constructor.java
@@ -2,8 +2,10 @@ package com.java.hello.module11;
 
 public class Constructor {
     public void foo(){
-        Flower flower = new Flower();
+        Flower flower = new Flower(); //способ описания объекта без конструктрора
         flower.name = "Ромашка";
         flower.color = "Желтая";
+
+        Flower secondFlower = new Flower("Роза", "Красная"); //Описание объекта с конструктором
     }
 }
