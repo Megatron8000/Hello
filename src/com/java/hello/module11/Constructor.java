d151ccaac59635a2bd8caad01544204e1f92964d
Megatron8000 <barykin88@hotmail.com>
2021-10-24 21:16:26 +0400

    write sample of Constructor

--- src/com/java/hello/module11/Constructor.java
+++ src/com/java/hello/module11/Constructor.java
@@ -3,5 +3,7 @@ package com.java.hello.module11;
 public class Constructor {
     public void foo(){
         Flower flower = new Flower();
+        flower.name = "Ромашка";
+        flower.color = "Желтая";
     }
 }
