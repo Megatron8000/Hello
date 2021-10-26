6a3f7f164d0ebcc0a54c191e8048598259c21e64
Megatron8000 <barykin88@hotmail.com>
2021-10-26 19:39:32 +0400

    learn modifiers

--- src/com/java/hello/module12/Modifiers.java
+++ src/com/java/hello/module12/Modifiers.java
@@ -3,5 +3,10 @@ package com.java.hello.module12;
 public class Modifiers {                // public          default          protected           private
         public String name;
         public Modifiers(){};
+
+
+                private class FirstClass{
+                    public String color;
+                }
         public void foo(){};
 }
