77219c61dbbd06effd1b332a1c50cdc3beb709ea
Megatron8000 <barykin88@hotmail.com>
2021-10-30 09:10:48 +0400

    start learn new theme

--- src/com/java/hello/module13/Bus.java
+++ src/com/java/hello/module13/Bus.java
@@ -1,4 +1,6 @@
 package com.java.hello.module13;
 
 public class Bus {
+    public String model, color;
+    public Engine engine;
 }
---
+++ src/com/java/hello/module13/Engine.java
@@ -0,0 +1,5 @@
+package com.java.hello.module13;
+
+public class Engine {
+    public String type;
+}
