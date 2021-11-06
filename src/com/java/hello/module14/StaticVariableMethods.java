76a464f5f62c21531250ef72c34d245e3fd15d43
Megatron8000 <barykin88@hotmail.com>
2021-11-06 11:09:01 +0400

    learn static modifier

--- src/com/java/hello/module14/StaticVariableMethods.java
+++ src/com/java/hello/module14/StaticVariableMethods.java
@@ -2,4 +2,13 @@ package com.java.hello.module14;
 
 public class StaticVariableMethods {
         public static int variable;   // общее поле для всех объектов данного класса
+        public int variable1; // не статические перенменные нельзя использовать в методах класса
+
+            public static void foo(){
+                System.out.println(variable);
+            }
+
+            public void show(){
+                System.out.println("Show must go on");
+            }
 }
