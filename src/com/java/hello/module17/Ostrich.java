5f0d39df3c8fb23793fc79c2b179d02257a4bf03
Megatron8000 <barykin88@hotmail.com>
2021-11-24 06:42:27 +0400

    personal ostrich class

--- src/com/java/hello/module17/Ostrich.java
+++ src/com/java/hello/module17/Ostrich.java
@@ -1,4 +1,7 @@
 package com.java.hello.module17;
 
-public class Ostrich extends Birds{
+public class Ostrich extends Birds{  //extends - наследование свойств указанного класса
+            public void hideHead(){
+                System.out.println("I ostrich and I scared");
+            }
 }
