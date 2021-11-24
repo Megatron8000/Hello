f5b5e65969bd5de7127d9f3b03e55c12b72212ed
Megatron8000 <barykin88@hotmail.com>
2021-11-24 06:55:43 +0400

    enable get/set methods for Birds

--- src/com/java/hello/module17/Birds.java
+++ src/com/java/hello/module17/Birds.java
@@ -8,4 +8,19 @@ public class Birds {
             System.out.println("Walk");
         }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Wings getWings() {
+        return wings;
+    }
+
+    public void setWings(Wings wings) {
+        this.wings = wings;
+    }
 }
