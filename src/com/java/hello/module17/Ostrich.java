7612f9ce2fc96706efc4eca3ee605a85db524f1e
Megatron8000 <barykin88@hotmail.com>
2021-11-26 06:50:33 +0400

    dynamic polymorphism

--- src/com/java/hello/module17/Ostrich.java
+++ src/com/java/hello/module17/Ostrich.java
@@ -7,7 +7,15 @@ public class Ostrich extends Birds{  //extends - наследование сво
                 System.out.println("I ostrich and I scared");
             }
 
+            /*пример статического полиморфизма и перегрузки методов,
+             в классе присутствуют два метода с однаковым именем, но разными принимаемыми значениями
+             */
             public void walk(String s){
-                System.out.println("Walk" + " " + s);  // пример статического полиморфизма и перегрузки методов, в классе присутствуют два метода с однаковым именем, но разными принимаемыми значениями
+                System.out.println("Walk" + " " + s);
             }
+
+    @Override
+    public void walk() {
+        System.out.println("an ostrich runs across the savannah");
+    }
 }
