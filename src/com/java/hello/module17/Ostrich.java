c857b6818e26dd661e4e9f937780e487ba898a2b
Megatron8000 <barykin88@hotmail.com>
2021-11-26 06:44:03 +0400

    added an overloaded method to the ostrich class

--- src/com/java/hello/module17/Ostrich.java
+++ src/com/java/hello/module17/Ostrich.java
@@ -3,11 +3,11 @@ package com.java.hello.module17;
 public class Ostrich extends Birds{  //extends - наследование свойств указанного класса
 
 
-    public Ostrich(String name, Wings wings) {
-        super(name, wings);
-    }
-
     public void hideHead(){
                 System.out.println("I ostrich and I scared");
+            }
+
+            public void walk(String s){
+                System.out.println("Walk" + " " + s);  // пример статического полиморфизма и перегрузки методов, в классе присутствуют два метода с однаковым именем, но разными принимаемыми значениями
             }
 }
