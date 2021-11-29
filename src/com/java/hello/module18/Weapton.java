9fb3191a48e0e5a7ecedef0498e80214bc3ee1c7
Megatron8000 <barykin88@hotmail.com>
2021-11-29 07:03:07 +0400

    learn abstract

--- src/com/java/hello/module18/Weapton.java
+++ src/com/java/hello/module18/Weapton.java
@@ -1,4 +1,7 @@
 package com.java.hello.module18;
 
-public abstract class Weapton {
+public abstract class Weapton {  //абстракция - общее обисание для группы классов
+    public String name;
+
+    public abstract void foo(); //абстрактные методы не имеют реализации и должны переопределяться в потомках
 }
