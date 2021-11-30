c3723f8dd8002e9fcaf396c97f14542a4effc7b7
Megatron8000 <barykin88@hotmail.com>
2021-11-30 07:23:23 +0400

    learn interfaces

--- src/com/java/hello/module19/Mi8.java
+++ src/com/java/hello/module19/Mi8.java
@@ -1,8 +1,13 @@
 package com.java.hello.module19;
 
-public class Mi8 extends FlyingObjects{
+public class Mi8 extends FlyingObjects implements VerticalTakeOf{
     @Override
     public void fly() {
-        System.out.println("heli MI8 fly");
+        System.out.println("helicopter MI8 fly");
+    }
+
+    @Override
+    public void verticalTakeOf() {
+        System.out.println("MI8 can verticalTakeOf");
     }
 }
--- src/com/java/hello/module19/VerticalTakeOf.java
+++ src/com/java/hello/module19/VerticalTakeOf.java
@@ -1,4 +1,5 @@
 package com.java.hello.module19;
 
-public interface VerticalTakeOf {
+public interface VerticalTakeOf { //поля интерфейса по умолчанию public static finally
+        public void verticalTakeOf();
 }
