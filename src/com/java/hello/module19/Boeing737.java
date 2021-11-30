0afcc82d8138c11e7f1c3a3efb3bf9b271794467
Megatron8000 <barykin88@hotmail.com>
2021-11-30 06:56:54 +0400

    learn interfaces

---
+++ src/com/java/hello/module19/Boeing737.java
@@ -0,0 +1,8 @@
+package com.java.hello.module19;
+
+public class Boeing737 extends Airplane{
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ src/com/java/hello/module19/Mi8.java
@@ -0,0 +1,8 @@
+package com.java.hello.module19;
+
+public class Mi8 extends Helicopter{
+    @Override
+    public void fly() {
+
+    }
+}
