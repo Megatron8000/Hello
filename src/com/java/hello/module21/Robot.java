b165a3c06ac23b3aad1cfda36cd0ac2bea34ec95
Megatron8000 <barykin88@hotmail.com>
2021-12-09 06:28:23 +0400

    learn generics

--- src/com/java/hello/module21/Robot.java
+++ src/com/java/hello/module21/Robot.java
@@ -1,6 +1,27 @@
 package com.java.hello.module21;
 
-public class Robot {
+public class Robot <T> {
     private Body body;
+    private T head;
 
+    public Robot(Body body, T head) {
+        this.body = body;
+        this.head = head;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public T getHead() {
+        return head;
+    }
+
+    public void setHead(T head) {
+        this.head = head;
+    }
 }
