da4647ac1ddb98391e82a418af0ada3f3415ed22
Megatron8000 <barykin88@hotmail.com>
2021-12-11 08:23:19 +0400

    1

--- src/com/java/hello/module21/SmallHead.java
+++ src/com/java/hello/module21/SmallHead.java
@@ -4,4 +4,9 @@ public class SmallHead extends Head{
     public void burn(){
         System.out.println("BURN");
     }
+
+    @Override
+    public void speak() {
+        System.out.println("I'm small head? and i speak german");
+    }
 }
