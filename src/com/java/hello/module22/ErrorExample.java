1e6f31b102f9215bf79820ee81b864fb8a9aad27
Megatron8000 <barykin88@hotmail.com>
2021-12-11 09:37:50 +0400

    1

--- src/com/java/hello/module22/ErrorExample.java
+++ src/com/java/hello/module22/ErrorExample.java
@@ -5,15 +5,18 @@ public class ErrorExample {
     public SomeClass someClass;
 
 
-   /* public void foo(){
+    public void foo(){
         System.out.println(1/0); //исключение типа RuntimeException, ошибка в арифметической операции
     }
     public void foo1(){
         int[] array = {1,2,3};
         array[5] = 6;   //исключение типа RuntimeException, индекс за пределами массива
-    }*/
-    public void foo2(){
-        someClass.foo(); //объект был объявлен, но не создан
     }
-
+    public void foo2(){
+        try {
+            someClass.foo();
+        } catch (Exception e){
+            System.err.println("Класс не создан");
+        }
+    }
 }
