dd78f4389d11a2f6557b15c9a1f3c41f7e29b146
Megatron8000 <barykin88@hotmail.com>
2021-12-21 07:13:18 +0400

    learn exceptions

--- src/com/java/hello/module23/SquarePerimeter.java
+++ src/com/java/hello/module23/SquarePerimeter.java
@@ -2,5 +2,8 @@ package com.java.hello.module23;
 
 public class SquarePerimeter {
 
-
+        public void getPerimeter() throws PerimeterException {
+            Square square = new Square();
+            square.setSide(5.5);
+        }
 }
