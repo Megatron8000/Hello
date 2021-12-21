481408dd43f9da3edcba1fd7005c4d9c23433f90
Megatron8000 <barykin88@hotmail.com>
2021-12-21 06:56:56 +0400

    learn exceptions

---
+++ src/com/java/hello/module23/Sqare.java
@@ -0,0 +1,16 @@
+package com.java.hello.module23;
+
+public class Sqare {
+    static double side;
+
+    public static double getSide() {
+        return side;
+    }
+
+    public static void setSide(double side) {
+        if(side<=0){
+
+        }
+        Sqare.side = side;
+    }
+}
