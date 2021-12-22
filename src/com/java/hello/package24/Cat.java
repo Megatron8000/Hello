adecbfdaa56b31b1f839c58ea4931440ea534748
Megatron8000 <barykin88@hotmail.com>
2021-12-22 07:18:02 +0400

    learn serialization

--- src/com/java/hello/package24/Cat.java
+++ src/com/java/hello/package24/Cat.java
@@ -1,4 +1,17 @@
 package com.java.hello.package24;
 
 public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
 }
